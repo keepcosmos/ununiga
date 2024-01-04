e900166e70b566033de0165ece54bc0cb5059b7d
Soyu Kim <mmx900@gmail.com>
2024-01-04 20:46:50 +0900

    support HTML translations

--- lib/ununiga/i18n/josa_transformer.rb
+++ lib/ununiga/i18n/josa_transformer.rb
@@ -2,13 +2,13 @@ require 'ununiga/josa_picker'
 
 module Ununiga::I18n
   module JosaTransformer
-    def translate(*args)
-      transform(super)
+    def translate(locale, key, options = {})
+      transform(super, key.end_with?("_html", ".html"))
     end
 
-    def transform(entry)
+    def transform(entry, is_html)
       if entry.is_a?(String) && I18n.locale.to_s =~ /ko|ko_KR/i
-        return Ununiga::JosaPicker.new(entry).takewell
+        return Ununiga::JosaPicker.new(entry, is_html).takewell
       end
       entry
     end
--- test/test_josa_transformer.rb
+++ test/test_josa_transformer.rb
@@ -15,12 +15,19 @@ class JosaTransformerTest < Minitest::Test
     assert_equal '철수가 돈을 냅니다.', I18n.t(:someone_pay, name: '철수')
     assert_equal '재현이 돈을 냅니다.', I18n.t(:someone_pay, name: '재현')
 
-
     assert_equal '호랑이는 사과를 먹습니다.', I18n.t(:someone_eat_something, name: '호랑이', meal: '사과')
     assert_equal '곰은 마늘을 먹습니다.', I18n.t(:someone_eat_something, name: '곰', meal: '마늘')
 
     assert_equal '재현과 진아는 개발을 합니다.', I18n.t(:do_something_with_someone, name1: '재현', name2: '진아', doing: '개발')
     assert_equal '정하와 민정은 디자인을 합니다.', I18n.t(:do_something_with_someone, name1: '정하', name2: '민정', doing: '디자인')
+  end
+
+  def test_transform_korean_with_html
+    assert_equal '<b>철수</b>가 돈을 냅니다.', I18n.t(:someone_pay_html, name: '철수')
+    assert_equal '<b>재현</b>이 돈을 냅니다.', I18n.t(:someone_pay_html, name: '재현')
+
+    assert_equal '<b>호랑이</b>는 <b>사과</b>를 먹습니다.', I18n.t("someone_eat_nesting.html", name: '호랑이', meal: '사과')
+    assert_equal '<b>곰</b>은 <b>마늘</b>을 먹습니다.', I18n.t("someone_eat_nesting.html", name: '곰', meal: '마늘')
   end
 
   def test_not_string_locale_value
